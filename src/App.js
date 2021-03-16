a66715a8989d677188c0062e51172b14f6025405
maatiasviola <matiasviola02@gmail.com>
2021-03-16 16:22:49 -0300

    anecdotes

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+  
+  const [selected, setSelected] = useState(0)
+  const [votes,setVotes]=useState(new Array(anecdotes.length).fill(0))
+
+  const handleClickNext=()=>{
+    setSelected(Math.floor(Math.random()*anecdotes.length))
+  }
+  
+  const handleClickVote=()=>{
+    const newVote =
+     [...votes]
+      newVote[selected] += 1
+
+      setVotes(newVote)     
+  }
+
+  const mostVoted=votes.indexOf(Math.max(...votes))
+  
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>Has {votes[selected]} votes</p>
+      <button onClick={handleClickVote}>vote</button><button onClick={handleClickNext}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVoted]}</p>
+      <p>Has {votes[mostVoted]} votes</p>
+    </div>
+  )
+}
+
+export default App
